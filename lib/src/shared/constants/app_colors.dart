fdce3967cda1790d5c039ed87b45983804ec0dca
Jongwoo Lim <limjongwoo1994@gmail.com>
2025-01-22 15:21:55 -0700

    add custom text style and colors from Figma design system

--- lib/src/shared/constants/app_colors.dart
+++ lib/src/shared/constants/app_colors.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // Start: Example
-  static const extraLightExample = Color(0xFFFFFFFF);
-  static const lightExample = Color(0xFFFFFFFF);
-  static const example = Color(0xFFFFFFFF);
-  static const darkExample = Color(0xFFFFFFFF);
-  static const extraDarkExample = Color(0xFFFFFFFF);
-  // End: Example
+  static const customLightBlack = Color(0xFF505050);
+  static const customBlack = Color(0xFF252525);
+
+  static const customLightGray = Color(0xFFE1E1E1);
+  static const customGray = Color(0xFFB5B5B5);
+
+  static const customWhite = Color(0xFFF9F9F9);
+
+  static const customLightPink = Color(0xFFF1ABBE); // disabled color
+  static const customPink = Color(0xFFE85B81);
+  static const customDarkPink = Color(0xFFC12F5D); // hover color
+
+  static const customLightPurple = Color(0xFFB5A1F1); // disabled color
+  static const customPurple = Color(0xFF5E48E8);
+  static const customDarkPurple = Color(0xFF5028C6); // hover color
+
+  static const customBlue = Color(0xFF8CE0F3);
 }
