ee5dfec51d4b9e868a0a49ef86fcc80bb5d1a73d
Jongwoo Lim <limjongwoo1994@gmail.com>
2025-01-22 16:37:32 -0700

    add screens for each navigation bar item

---
+++ lib/src/features/garage/presentation/garage_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class GarageScreen extends StatelessWidget {
+  const GarageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('garage screen'),
+    );
+  }
+}
