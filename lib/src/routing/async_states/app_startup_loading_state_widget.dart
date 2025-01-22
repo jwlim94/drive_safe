60d508d1ed4ff22d6bb12a31dbc64478d6736020
Jongwoo Lim <59632133+jwlim94@users.noreply.github.com>
2025-01-22 15:53:52 -0700

    add initial splash screen (#3)

--- lib/src/routing/async_states/app_startup_loading_state_widget.dart
+++ lib/src/routing/async_states/app_startup_loading_state_widget.dart
@@ -1,14 +1,15 @@
+import 'package:drive_safe/src/shared/constants/text_styles.dart';
 import 'package:flutter/material.dart';
 
+// Splash screen
 class AppStartupLoadingStateWidget extends StatelessWidget {
   const AppStartupLoadingStateWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: CircularProgressIndicator(),
+    return const Scaffold(
+      body: Center(
+        child: Text('Drive Safe', style: TextStyles.h1),
       ),
     );
   }
