f7894484078206625b879c9605934dcfb21f4351
Haneul-Song <haneulisstudying@gmail.com>
2025-02-09 23:50:02 -0700

    Mockup data updataed

--- lib/src/features/example/data/mock_friends.dart
+++ lib/src/features/example/data/mock_friends.dart
@@ -4,7 +4,7 @@ final List<Map<String, String>> mockFriends = [
     'phone': '987-654-321',
     'description': 'Buckin’ Bronco',
     'initial': 'R',
-    'avatarColor': 'blue'
+    'avatarColor': 'yellow'
   },
   {
     'name': 'Hannah',
@@ -17,6 +17,27 @@ final List<Map<String, String>> mockFriends = [
     'name': 'Alex',
     'phone': '222-333-444',
     'description': 'Turbo Charger',
+    'initial': 'A',
+    'avatarColor': 'green'
+  },
+  {
+    'name': 'Sky',
+    'phone': '111-222-333',
+    'description': 'Ha Ha Ha',
+    'initial': 'A',
+    'avatarColor': 'green'
+  },
+  {
+    'name': 'Jake',
+    'phone': '444-555-666',
+    'description': 'New Car',
+    'initial': 'A',
+    'avatarColor': 'green'
+  },
+  {
+    'name': 'John',
+    'phone': '777-888-999',
+    'description': 'Kia The Best',
     'initial': 'A',
     'avatarColor': 'green'
   },
