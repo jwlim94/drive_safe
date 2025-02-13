4a9ebe35533d8820d2f091d72e98af32dbad7d2a
Jongwoo Lim <limjongwoo1994@gmail.com>
2025-02-12 23:16:40 -0700

    add colors to numbers

--- lib/src/shared/constants/numbers.dart
+++ lib/src/shared/constants/numbers.dart
@@ -1,6 +1,46 @@
 class Numbers {
-  // Start: Example
-  static const int exampleInteger = 0;
-  static const double exampleDouble = 0.0;
-  // End: Example
+  // User Color
+  static const int userPrimaryRed = 0xFFE85B5B;
+  static const int userPrimaryOrange = 0xFFE8965B;
+  static const int userPrimaryYellow = 0xFFE8DA5B;
+  static const int userPrimaryGreen = 0xFF5BE873;
+  static const int userPrimaryBlue = 0xFF8CE0F3;
+  static const int userPrimaryPurple = 0xFF5E5BE8;
+  static const int userPrimaryPink = 0xFFE55BE8;
+  static const int userPrimaryBlack = 0xFF7B7B7B;
+  static const int userPrimaryWhite = 0xFFFFFFFF;
+
+  static const int userSecondaryRed = 0xFF932222;
+  static const int userSecondaryOrange = 0xFF934A22;
+  static const int userSecondaryYellow = 0xFF937922;
+  static const int userSecondaryGreen = 0xFF319322;
+  static const int userSecondaryBlue = 0xFF229391;
+  static const int userSecondaryPurple = 0xFF2D2293;
+  static const int userSecondaryPink = 0xFF932286;
+  static const int userSecondaryBlack = 0xFF252525;
+  static const int userSecondaryWhite = 0xFFB5B5B5;
+
+  static final List<int> userPrimaryColors = [
+    Numbers.userPrimaryRed,
+    Numbers.userPrimaryOrange,
+    Numbers.userPrimaryYellow,
+    Numbers.userPrimaryGreen,
+    Numbers.userPrimaryBlue,
+    Numbers.userPrimaryPurple,
+    Numbers.userPrimaryPink,
+    Numbers.userPrimaryBlack,
+    Numbers.userPrimaryWhite,
+  ];
+
+  static final List<int> userSecondaryColors = [
+    Numbers.userSecondaryRed,
+    Numbers.userSecondaryOrange,
+    Numbers.userSecondaryYellow,
+    Numbers.userSecondaryGreen,
+    Numbers.userSecondaryBlue,
+    Numbers.userSecondaryPurple,
+    Numbers.userSecondaryPink,
+    Numbers.userSecondaryBlack,
+    Numbers.userSecondaryWhite,
+  ];
 }
