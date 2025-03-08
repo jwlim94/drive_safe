b544962c8a92eb067556776a8a8fd24040bc79b1
jakerg@byu.edu <jakerg@byu.edu>
2025-03-08 09:51:37 -0700

    import one package...It got lost in the merge.

--- lib/src/features/user/presentation/controllers/update_user_friends_controller.dart
+++ lib/src/features/user/presentation/controllers/update_user_friends_controller.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:drive_safe/src/features/user/data/users_repository.dart';
+import 'package:drive_safe/src/features/user/domain/user.dart';
 import 'package:drive_safe/src/features/user/presentation/providers/current_user_state_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
