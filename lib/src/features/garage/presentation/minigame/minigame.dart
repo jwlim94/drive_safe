92e8c5a64e555db869d22548b894af117114f735
Jongwoo Lim <limjongwoo1994@gmail.com>
2025-03-09 20:55:19 -0600

    apply new screens

--- lib/src/features/garage/presentation/minigame/minigame.dart
+++ lib/src/features/garage/presentation/minigame/minigame.dart
@@ -1,24 +1,93 @@
-import 'package:drive_safe/src/features/garage/presentation/minigame/constants/constants.dart';
-import 'package:drive_safe/src/features/garage/presentation/minigame/players/player_stats.dart';
-import 'package:drive_safe/src/features/garage/presentation/minigame/world/rival_racing_world.dart';
-import 'package:drive_safe/src/shared/constants/app_colors.dart';
+import 'package:drive_safe/src/features/garage/presentation/minigame/routes/gameplay.dart';
+import 'package:drive_safe/src/features/garage/presentation/minigame/routes/main_menu.dart';
+import 'package:drive_safe/src/features/garage/presentation/minigame/routes/pause_menu.dart';
+import 'package:drive_safe/src/features/garage/presentation/minigame/routes/retry_menu.dart';
+import 'package:drive_safe/src/features/garage/presentation/minigame/routes/settings.dart';
 import 'package:flame/events.dart';
-import 'package:flame/game.dart';
-import 'package:flame/components.dart';
-import 'package:flame_riverpod/flame_riverpod.dart';
-import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flame/game.dart' as flame;
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-class RacingGame extends FlameGame {
+class RacingGame extends flame.FlameGame with HasKeyboardHandlerComponents {
+  final musicValueNotifier = ValueNotifier(true);
+  final sfxValueNotifier = ValueNotifier(true);
+
+  late final _routes = <String, flame.Route>{
+    MainMenu.id: flame.OverlayRoute(
+      (context, game) => MainMenu(
+        onPlayPressed: _startGame,
+        onSettingsPressed: () => _routeById(Settings.id),
+      ),
+    ),
+    Settings.id: flame.OverlayRoute(
+      (context, game) => Settings(
+        onBackPressed: _popRoute,
+        musicValueListenable: musicValueNotifier,
+        sfxValueListenable: sfxValueNotifier,
+        onMusicValueChanged: (value) => musicValueNotifier.value = value,
+        onSfxValueChanged: (value) => sfxValueNotifier.value = value,
+      ),
+    ),
+    PauseMenu.id: flame.OverlayRoute(
+      (context, game) => PauseMenu(
+        onResumePressed: _resumeGame,
+        onRestartPressed: _startGame,
+        onExitPressed: _exitToMainMenu,
+      ),
+    ),
+    RetryMenu.id: flame.OverlayRoute(
+      (context, game) => RetryMenu(
+        onRetryPressed: _startGame,
+        onExitPressed: _exitToMainMenu,
+      ),
+    ),
+  };
+
+  late final _router = flame.RouterComponent(
+    initialRoute: MainMenu.id,
+    routes: _routes,
+  );
+
   @override
   Future<void> onLoad() async {
-    final map = await TiledComponent.load("desert_track.tmx", Vector2.all(16));
-    await add(map);
+    await add(_router);
   }
 
-  @override
-  Color backgroundColor() {
-    return AppColors.customLightBlack;
+  void _routeById(String id) {
+    _router.pushNamed(id);
+  }
+
+  void _popRoute() {
+    _router.pop();
+  }
+
+  void _startGame() {
+    _router.pushReplacement(
+      flame.Route(
+        () => Gameplay(
+          onPausePressed: _pauseGame,
+          onGameOver: _showRetryMenu,
+        ),
+      ),
+      name: Gameplay.id,
+    );
+  }
+
+  void _pauseGame() {
+    _router.pushNamed(PauseMenu.id);
+    pauseEngine();
+  }
+
+  void _resumeGame() {
+    _router.pop();
+    resumeEngine();
+  }
+
+  void _exitToMainMenu() {
+    _resumeGame();
+    _router.pushReplacementNamed(MainMenu.id);
+  }
+
+  void _showRetryMenu() {
+    _router.pushNamed(RetryMenu.id);
   }
 }
--- lib/src/features/garage/presentation/minigame_screen.dart
+++ lib/src/features/garage/presentation/minigame_screen.dart
@@ -1,24 +1,14 @@
 import 'package:drive_safe/src/features/garage/presentation/minigame/minigame.dart';
 import 'package:flame/game.dart';
-import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
-
-final gameInstance = RacingGame();
-final GlobalKey<RiverpodAwareGameWidgetState> gameWidgetKey =
-    GlobalKey<RiverpodAwareGameWidgetState>();
 
 class RacingGameScreen extends StatelessWidget {
   const RacingGameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Rival Racing'),
-      ),
-      body: const GameWidget.controlled(
-        gameFactory: RacingGame.new,
-      ),
+    return const Scaffold(
+      body: GameWidget.controlled(gameFactory: RacingGame.new),
     );
   }
 }
