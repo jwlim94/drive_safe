94f9c710ebf8cdbd7ecf10c3d8861b7f5758f245
Jongwoo Lim <limjongwoo1994@gmail.com>
2025-03-10 02:33:06 -0600

    edit assets to simple roads first

--- assets/tiles/Desert_gas_station (128 x 304).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Desert_gas_station (128 x 304)" tilewidth="32" tileheight="32" tilecount="36" columns="4">
- <image source="../images/Desert_gas_station (128 x 304).png" width="128" height="304"/>
-</tileset>
