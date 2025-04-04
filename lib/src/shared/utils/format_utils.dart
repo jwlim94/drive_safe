c96e9e5435d6c0d97cdb1bb6f8773caaa9182b6a
jakerg@byu.edu <jakerg@byu.edu>
2025-04-03 19:03:41 -0600

    minor bug fix for potential errors in code formatting.

--- lib/src/shared/utils/format_utils.dart
+++ lib/src/shared/utils/format_utils.dart
@@ -1,5 +1,7 @@
 class FormatUtils {
   static String formatUserCode(String userCode) {
+    userCode = userCode.padRight(
+        12, '0'); //Pad strings in case anything goes wrong during generation.
     return "${userCode.substring(0, 4)}-${userCode.substring(4, 8)}-${userCode.substring(8, 12)}";
   }
 }
